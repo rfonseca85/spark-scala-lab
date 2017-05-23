37b562e74c3b752a0e4e31ceccb5b340283640c3
Rafael Fonseca <rfonseca85@yahoo.ca>
2017-05-22 21:00:36 -0400

    First Commit

---
+++ src/main/scala/com/sn0wB/sparklab/Test.scala
@@ -0,0 +1,12 @@
+package com.devinline.spark
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD.rddToPairRDDFunctions
+
+object Test {
+
+  def main(args: Array[String]): Unit = {
+
+
+  }
+}
