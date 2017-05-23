d47327f030ea97ca84c2c6077a2daf4c423e5465
Rafael Fonseca <rfonseca85@yahoo.ca>
2017-05-23 16:55:22 -0400

    Changes

---
+++ src/main/scala/com/sn0wB/sparklab/PiEstimation.scala
@@ -0,0 +1,34 @@
+package com.sn0wB.sparklab
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+
+object PiEstimation {
+  
+  def main(args: Array[String]): Unit = {
+    
+        //Start the Spark context
+    val conf = new SparkConf()
+      .setAppName("WordCount")
+      .setMaster("local")
+    val sc = new SparkContext(conf)
+
+    //Read some example file to a test RDD
+    val test = sc.textFile("input.txt")
+
+    test.flatMap { line => //for each line
+      line.split(" ") //split the line in word by word.
+    }
+      .map { word => //for each word
+        (word, 1) //Return a key/value tuple, with the word as key and 1 as value
+      }
+      .reduceByKey(_ + _) //Sum all of the value with same key
+      .foreach(x => println(x))
+
+    //Stop the Spark context
+    sc.stop
+    
+  }
+  
+  
+}
